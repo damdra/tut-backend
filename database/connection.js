0c2bbc09a68b4f5408ce64f957d505b8fd6e4cd8
Dreamer <damdra604@gmail.com>
2022-09-18 19:11:07 +0500

    addding

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ module.exports = async () => {
     try {
      await mongo.connect(process.env.DB_URL, {useNewUrlParser: true});    
      console.log("database connection established")
-    } catch (error) {
+    } catch (error) {   
         console.log("databse connection error: ", error)
         throw new Error(error)
     }
